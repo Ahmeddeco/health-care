e8ec7652607454bb272b5e66f9e07cb9d24bd2ad
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-09-05 17:54:09 +0300

    main section start

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+import PatientForm from '@/components/forms/PatientForm'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function HomePage() {
 	return (
-		<main className='flex mx-auto items-center justify-center h-screen text-7xl  font-extrabold'>
-			Health Care
+		<main className='flex h-screen max-h-screen '>
+			<section className='remove-scrollbar container my-auto'>
+				<div className='sub-container max-w-[496px]'>
+					<Image
+						src='/assets/icons/logo-full.svg'
+						alt='patient'
+						width={1000}
+						height={1000}
+						className='mb-12 h-10 w-fit'
+					/>
+					<PatientForm />
+					<div className='text-14-regular mt-20 justify-between '>
+						<p className='justify-items-end text-dark-600 xl:text-left'>
+							© 2024 Health Care
+						</p>
+						<Link href='/?admin=true' className='text-green-500'>
+							Admin
+						</Link>
+					</div>
+				</div>
+			</section>
+			<Image
+				src='/assets/images/onboarding-img.png'
+				alt='hero'
+				width={1000}
+				height={1000}
+				className='side-img max-w-[50%]'
+			/>
 		</main>
 	)
 }
